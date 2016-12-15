88b16157d87f5f183e8fcc9837490b1d6b5a1db4
ChrisJamesC <christopher.chiche@gmail.com>
2016-12-15 12:11:47 +0100

    Finish first pass on refactoring

--- app/directive/layoutmodel.js
+++ app/directive/layoutmodel.js
@@ -71,13 +71,6 @@ angular.module('layoutmodel', [ 'lodash', 'ui.bootstrap' ])
                     }
                     return i;
                 };
-
-                scope.classes = function(data, header) {
-                    /*jshint eqnull:true */
-                    var add = header.IsRollUp ? ' yrollupdata' : '';
-                    add += (header.Depth >= 3 && header.IsRollUp) ? ' subyrollupdata' : '';
-                    return add;
-                };
                 /* End of CSS class definitions */
 
                 scope.isVisible = function(header) {
@@ -106,8 +99,6 @@ angular.module('layoutmodel', [ 'lodash', 'ui.bootstrap' ])
                 scope.dataTemplate = scope.dataTemplateUrl || 'defaultData.html';
                 scope.headerTemplate = scope.headerTemplateUrl || 'defaultHeader.html';
 
-                var tableIndex = 0;
-
                 scope.getConstraintLabel = (pair,index) => {
                     var value = _.toPairs(pair)[0][index]
                     const label = _.get(scope.constraintLabels,value,value);
@@ -120,20 +111,30 @@ angular.module('layoutmodel', [ 'lodash', 'ui.bootstrap' ])
                         return "";
                     }
                 }
+                scope.bodyHeaders = (facts, factsIdx) => _(scope.yHeaders)
+                    .filter((d,ii) => ii>0)
+                    .map((groupCells, index) =>
+                        _.filter(groupCells, (cell, groupCellIndex) => {
+                            const sum = _(groupCells)
+                                .filter((c,ii) => ii<groupCellIndex )
+                                .map(c => c.CellSpan || 1)
+                                .sum();
+                            return (factsIdx===0 && sum===0) || (factsIdx>0 && factsIdx===sum);
+                        })
+                    )
+                    .flatten()
+                    .value();
+
 
                 scope.$watch(function() { return scope.layoutModel; }, function() {
                     // Data not yet available?
-                    // if (!scope.layoutModel) {
-                    //     scope.table = [];
-                    //     scope.yHeaderGroups = [];
-                    //     scope.xHeaderGroups = [];
-                    //     scope.data = [];
-                    //     return;
-                    // }
+                    if (!scope.layoutModel) {
+                        return;
+                    }
 
                     // Check if this component may be used to render the table
                     if (scope.layoutModel.ModelKind !== 'LayoutModel' || !scope.layoutModel.TableSet) { throw new Error('layoutmodel: "model" parameter does not contain a layout model!'); }
-                    if (scope.layoutModel.TableSet.length <= tableIndex) { throw new Error('layoutmodel: No table '+tableIndex+' in TableSet'); }
+                    if (scope.layoutModel.TableSet.length <= 0) { throw new Error('layoutmodel: No table '+tableIndex+' in TableSet'); }
 
                     scope.header = scope.layoutModel.ComponentAndHypercubeInformation;
                     scope.constraints = scope.layoutModel.GlobalConstraints;
@@ -156,111 +157,6 @@ angular.module('layoutmodel', [ 'lodash', 'ui.bootstrap' ])
                     if (scope.table.TableHeaders.z) { console.error('layoutmodel: Z-Axis not supported.'); }
                     var ao = scope.table.TableCells.AxisOrder;
                     if (!ao || ao.length!==2 || ao[0] !== 'y' || ao[1] !== 'x') { throw new Error('layoutmodel: table cells must be in y, x axis order.'); }
-
-                    // End of sanity
-
-
-                    scope.yHeaderGroups = [];
-                    scope.ySuperHeaders = []; // y super headers will be put in the top left corner
-                    scope.xHeaderGroups = [];
-                    scope.data = scope.table.TableCells.Facts;
-
-                    var rows = 0;
-
-                    // count the max rows
-                    _.each(scope.table.TableHeaders.y, function(y){
-                        _.each(y.GroupCells, function(groupCells){
-                            // calculate total rows for all row header column
-                            var span = _.reduce(groupCells, function(total, cell){
-                                return total + (cell.CellSpan || 1);
-                            }, 0);
-                            if(span > rows){
-                                rows = span;
-                            }
-                        });
-                    });
-
-                    // Build columns in required order
-                    _.each(scope.table.TableHeaders.x, function(x){
-                        _.each(x.GroupCells, function(grp){
-                            scope.xHeaderGroups.push(grp);
-                        });
-                    });
-
-                    // build rows in required order
-                    var allHeaderGroupCells = [];
-                    _.each(scope.table.TableHeaders.y, function(y) {
-                        _.each(y.GroupCells, function(grp) {
-                            allHeaderGroupCells.push(grp);
-                        });
-                    });
-
-                    // a supergroup is a group of columns under a superheader
-                    // a superheader spans all rows and can therefore be moved
-                    // in the top left corner
-                    var groupIdx = 0;
-                    var superGroups = _.groupBy(allHeaderGroupCells, function(grp){
-                        if(grp[0].CellSpan === rows){
-                            grp[0].IsSuperHeader = true;
-                            groupIdx++;
-                        }
-                        return groupIdx;
-                    });
-
-                    // if all headers are supergroups, nothing is displayed
-                    // for this reason we need to do this fix:
-                    superGroups = _.values(superGroups);
-                    if(superGroups.length === allHeaderGroupCells.length){
-                        superGroups = [ _.flatten(superGroups) ];
-                    }
-
-                    _.each(superGroups, function(superColumns){
-                        _.each(superColumns, function(col, i){
-                            var rowIdx = 0;
-                            _.each(col, function(cell, j){
-
-                                // if the first cell in the first col of a
-                                // superColumns group is a superheader we move
-                                // it up in the super header top left corner
-                                if(i === 0 && j === 0 && cell.IsSuperHeader === true){
-                                    scope.ySuperHeaders.push({
-                                        Label: cell.CellLabels[0],
-                                        ColSpan: superColumns.length -1
-                                    });
-                                } else {
-
-                                    // if the first cell in the first col of a
-                                    // superColumns group is not a superheader
-                                    // then we need to add a placeholder
-                                    if(i === 0 && j === 0 && cell.IsSuperHeader !== true){
-                                        scope.ySuperHeaders.push({
-                                            Label: '',
-                                            ColSpan: superColumns.length -1
-                                        });
-                                    }
-
-                                    // add row
-                                    if(scope.yHeaderGroups.length <= rowIdx)
-                                    {
-                                        scope.yHeaderGroups.push([]);
-                                    }
-
-                                    if(_.isEmpty(cell.CellLabels)) {
-                                        cell.CellLabels.push('');
-                                        cell.RollUp = false;
-                                    }
-
-                                    scope.yHeaderGroups[rowIdx].push(cell);
-                                    rowIdx += (cell.CellSpan || 1);
-                                    // fill up with empty rows cols if cellspan > 1
-                                    while (scope.yHeaderGroups.length < rowIdx)
-                                    {
-                                        scope.yHeaderGroups.push([{ CellLabels: [] }]);
-                                    }
-                                }
-                            });
-                        });
-                    });
                 });
 
             }
